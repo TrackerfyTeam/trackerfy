e45c8a516555f1bab52beb95c0edc00d3a43e7dd
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-04-13 00:35:43 -0300

    Update app.js

--- spotify/app.js
+++ spotify/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const SpotifyWebApi = require('spotify-web-api-node');
 const router = express.Router();
 const functions = require('./src/js/modules.js');
 const querystring = require('querystring');
@@ -10,30 +9,26 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const redirect_uri = 'http://localhost:3000/callback';
 
-// credentials are optional
-const spotifyApi = new SpotifyWebApi({
-  clientId: clientId,
-  clientSecret: clientSecret,
-  redirectUri: redirect_uri
-});
-
-
 app.get('/login', function(req, res) {
 
   const state = functions.randomString(16);
   const scope = 'user-read-private user-read-email';
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: clientId,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        querystring.stringify({
+            response_type: 'code',
+            client_id: clientId,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
     }));
 });
+
+app.get('/callback', (req, res) => {
+    res.send(req.query.code)
+})
 
 app.use('/', router);
 app.listen(3000, () => {
     console.log('aplication running!');
-})
+});
