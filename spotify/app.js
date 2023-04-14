d9fbc53a8214317aaa5a56a09ba24a03485438ce
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-04-14 16:13:42 -0300

    teste1

--- spotify/app.js
+++ spotify/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+app.use(express.static('public'));
+app.use(express.json({limit: "1mb"}));
+
+const Datastore = require('nedb');
+
 const querystring = require('querystring');
+
 const SpotifyWebApi = require('spotify-web-api-node');
+
 const https = require('https');
+
+const database = new Datastore('database.db');
+database.loadDatabase();
 
 require('dotenv').config();
 const CLIENT_ID = process.env.CLIENT_ID;
@@ -53,7 +62,7 @@ app.get('/login', (req, res) => {
     
     // Rota para a página de redirecionamento após a autorização
 app.get('/callback', (req, res) => {
-        const code = req.query.code;
+    const code = req.query.code;
   
     // Configure os dados da solicitação de token
     const data = querystring.stringify({
@@ -63,98 +72,32 @@ app.get('/callback', (req, res) => {
     });
   
     // Configure as opções da solicitação de token
-    const options = {
-        hostname: 'accounts.spotify.com',
-        path: '/api/token',
+    fetch('https://accounts.spotify.com/api/token', {
         method: 'POST',
         headers: {
             'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
             'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(data)
-        }
-    };
-  
-    // Envie a solicitação de token
-    const tokenReq = https.request(options, tokenRes => {
-        tokenRes.setEncoding('utf8');
-        let body = '';
-        tokenRes.on('data', chunk => {
-            body += chunk;
-        });
-        tokenRes.on('end', () => {
-        const tokenData = JSON.parse(body);
-        console.log(tokenData);
+        },
+        body: data
+    })
+    .then(response => response.json())
+    .then(tokenData => {
         // Faça algo com o token de acesso retornado
-        res.send(tokenData);
-        spotifyApi.setAccessToken(tokenData.access_token);
-        });
+        database.insert(tokenData);
+        console.log(tokenData);
+    })
+    .catch(error => {
+        console.error('Erro ao buscar token de acesso:', error);
     });
-
-   
-
-    spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar')
-    .then(function(data) {
-    console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
-    }, function(err) {
-    console.log('Something went wrong!', err);
-    });
-  
-    tokenReq.on('error', err => {
-      console.error(err);
-    });
-  
-    tokenReq.write(data);
-    tokenReq.end();
 });
-  
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-})
 
-app.use('/', router);
+app.get('/api', (req, res) => {
+    database.find({}, (err, data) => {
+        res.json(data);
+    });
+});
+
 app.listen(3000, () => {
     console.log('aplication running!');
 });
 
-
-
-// router.get('/login', (req, res, next) => {
-//     res.redirect(spotifyApi.createAuthorizeURL([
-//         'ugc-image-upload',
-//         'user-read-playback-state',
-//         'user-modify-playback-state',
-//         'user-read-currently-playing',
-//         'app-remote-control',
-//         'streaming',
-//         'playlist-read-private',
-//         'playlist-read-collaborative',
-//         'playlist-modify-private',
-//         'playlist-modify-public',
-//         'user-follow-modify',
-//         'user-follow-read',
-//         'user-read-playback-position',
-//         'user-top-read',
-//         'user-read-recently-played',
-//         'user-library-modify',
-//         'user-library-read',
-//         'user-read-email',
-//         'user-read-private'
-//     ]))
-// });
-
-// router.get('/callback', async (req, res, next) => {
-//     const code = req.query.code;
-//     spotifyApi.authorizationCodeGrant(code).then((response) => {
-//         res.send(JSON.stringify(response));
-//         let token = response.body.access_token;
-//         spotifyApi.setAccessToken(token);
-
-//         spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar')
-//         .then(function(data) {
-//         console.log('Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name', data.body);
-//         }, function(err) {
-//         console.log('Something went wrong!', err);
-//         });
-//     });
-// });
