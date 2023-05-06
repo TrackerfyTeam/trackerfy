c79f0b67c90e80f8229434028a28476156a94a91
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-05-06 01:53:19 -0300

    testes

--- spotify/app.js
+++ spotify/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes');
 
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch');
 
-app.use(express.urlencoded({ extended: true}));
+require('dotenv').config();
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: client_id,
+    clientSecret: client_secret,
+    redirectUri: "http://localhost:3000/callback",
+})
 
 app.use(express.static('public'));
+app.use(express.json({limit: "1mb"}));
 
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    res.render('login');
+})
 
 app.get('/callback', async (req, res) => {
     
@@ -21,8 +36,7 @@ app.get('/callback', async (req, res) => {
         
         spotifyApi.getPlaylist('37i9dQZEVXbMDoHDwVN2tF')
             .then(function(data) {
-                const dataejs = { body: JSON.stringify(data.body.tracks.items[6].track.artists[0].name)}
-                res.render('year', { dataejs });
+                res.render('year');
                 console.log(data.body.tracks.items[6].track.artists[0].name);
             }, function(err) {
                 console.log('Something went wrong!', err);
