6817d6141c2fbdc0cac5d0e98f54341830a572fb
CycleWs <joaohlspj@gmail.com>
2023-05-09 16:08:39 -0300

    DATABASE TRACKERFY FOI?

--- spotify/database/database.js
+++ spotify/database/database.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
+const fs = require('fs');
 require('dotenv').config();
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    connectTimeout: 5000
+    connectTimeout: 5000,
+    ssl: {
+        ca: fs.readFileSync('/Users/CycleWs/Desktop/DigiCertGlobalRootCA.crt.pem')
+      }
 });
 
 function getPlaylistId(x){
@@ -23,7 +27,7 @@ function insertData(y){
     connection.connect((err)=>{
         if(err) throw err;
         let sql2 = `INSERT INTO accesstokendb(actk) VALUES ("${y}");`
-        con.query( sql2, function(err, res){
+        connection.query( sql2, function(err, res){
             if(err) throw err;
         })
     })
@@ -33,7 +37,7 @@ function deleteData(z){
     connection.connect((err)=>{
         if(err) throw err;
         let sql3 = `DELETE FROM access_token WHERE access_token = '${z}'`
-        con.query(sql3,function(err,res){
+        connection.query(sql3,function(err,res){
             if(err) throw err;
         })
     })
