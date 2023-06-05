a724bd39c084434c20e179cb9bff30a3cc63d243
Ícaro Cedraz <icarocedraz7@gmail.com>
2023-06-05 00:47:11 -0300

    Update routes.js

--- spotify/routes.js
+++ spotify/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const spotifyApi = require('./spotifyApi');
 const db = require('./database');
 
-router.get('/', async (req, res) => {
+router.get('https://trackerfy.onrender.com/', async (req, res) => {
     res.render('login');
 });
 
